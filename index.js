3d16c81ca274f72795f97d853719324cd670ea95
dmp42 <olivier@webitup.fr>
2014-02-05 19:20:20 +0100

    Updated parsers and generators integration

--- index.js
+++ index.js
@@ -9,49 +9,43 @@
   var File = gutil.File;
   var PluginError = gutil.PluginError;
 
-  var parser = require('./lib/parser.js');
-  var builder = require('./lib/generator.js');
+  var Parser = require('./lib/parser.js');
+  var Generator = require('./lib/generator.js');
 
   /**
    * That's the plugin parser
    */
 
-  var streamParser = function (name, options) {
+  var streamParser = function (options, name) {
     name = name || 'yuidoc.json';
-    options = options || {};
-    options.syntaxtype = options.syntaxtype || 'js';
 
-    var filemap = {};
-    var dirmap = {};
     var firstFile = null;
+
+    var wp = new Parser(options);
 
     var bufferFiles = function(file, enc, next){
       if (file.isNull()) return; // ignore
       if (file.isStream()) return this.emit('error', new PluginError('gulp-yuidoc',  'Streaming not supported'));
 
       // Store firstFile to get a base and cwd later on
-      if (!firstFile) firstFile = file;
+      if (!firstFile)
+        firstFile = file;
 
-      // Build-up the filemap and dirmap to later feed the parser
-      filemap[file.path] = file.contents.toString('utf8');
-      dirmap[file.path] = file.cwd;
+      wp.parse(file);
 
       next();
     };
 
     var endStream = function(conclude){
-      // Nothing? Cut the chase!
-      if (Object.keys(filemap).length === 0) return conclude();
-
       var data;
       try{
-        data = JSON.stringify(parser(options, filemap, dirmap));
+        data = JSON.stringify(wp.complete());
+        // data = parser(options, filemap));
       }catch(e){
-        return this.emit('error', new PluginError('gulp-yuidoc',
-          'Oooooh! Failed parsing with yuidoc. What did you do?!' + e));
+        return this.emit('error', new PluginError('gulp-jsdoc',
+          'Oooooh! Failed parsing with yuisdoc. What did you do?! ' + e));
       }
 
-      // console.warn(data);
       // Pump-up the generated json file
       this.push(new File({
         cwd: firstFile.cwd,
@@ -68,72 +62,51 @@
   };
 
   // May take any number of documentation json files as entry - this is kind of stupid, but well
-  var streamBuilder = function(options){
+  var streamGenerator = function(options){
     options = options || {};
+    var gp = new Generator(options);
 
     var processor = function(file, enc, next){
       if (file.isNull()) return; // ignore
-      if (file.isStream()) return this.emit('error', new PluginError('gulp-yuidoc',  'Streaming not supported'));
+      if (file.isStream()) return this.emit('error', new PluginError('gulp-jsdoc',  'Streaming not supported'));
 
-      var json;
-      try{
-        json = JSON.parse(file.contents.toString('utf8'));
-      }catch(e){
-        return this.emit('error', new PluginError('gulp-yuidoc', 'Generator: invalid input documentation data!' + e));
-      }
 
       // Globs the assets from the default theme
       var theme = path.join(__dirname, 'node_modules', 'yuidocjs', 'themes', 'default');
       var opt = {cwd: theme};
       var defaultheme = './assets/**/*';
 
-      // Build from JSON
-      builder(options, json, function(result){
+      try{
+        gp.render(file, function(result){
+          // Get the path/content pairs and spoof them into the stream
+          Object.keys(result).forEach(function(f){
+            var p = path.resolve(file.cwd, f);
+            this.push(new File({
+              cwd: file.cwd,
+              base: file.cwd,
+              path: p,
+              contents: new Buffer(result[f])
+            }));
+          }.bind(this));
 
-        // Get the path/content pairs and spoof them into the stream
-        Object.keys(result).forEach(function(f){
-          var p = path.resolve(file.cwd, f);
-          this.push(new File({
-            cwd: file.cwd,
-            base: file.cwd,
-            path: p,
-            contents: new Buffer(result[f])
-          }));
+          // Get the theme and spoof it into us stream
+          fs.src(defaultheme, opt)
+            .pipe(through2.obj(function(f, e, n){
+              // XXX why oh why - vinyl doesn't make much sense :-(
+              f.base = f.cwd;
+              this.push(f);
+              n();
+            }.bind(this), function(end){
+              end();
+              // XXX get the custom theme, possibly, otherwise, end the main stream as well.
+              next();
+            }));
         }.bind(this));
+      }catch(e){
+        return this.emit('error', new PluginError('gulp-jsdoc',
+          'Oooooh! Failed rendering with yuidoc. What did you do?! ' + e));
+      }
 
-        // Get the theme and spoof it into us stream
-        fs.src(defaultheme, opt)
-          .pipe(through2.obj(function(f, e, n){
-            // XXX why oh why - vinyl doesn't make much sense :-(
-            f.base = f.cwd;
-            this.push(f);
-            n();
-          }.bind(this), function(end){
-            end();
-            // XXX get the custom theme, possibly, otherwise, end the main stream as well.
-            next();
-          }));
-      }.bind(this));
-
-     //  var globber = new glob.Glob(path, opt);
-
-     //  // globber.on('error', this.emit.bind(this, 'error'));
-
-     //  // Push the shit inside
-     //  globber.on('match', function(filename) {
-     //    var p = path.resolve(opt.cwd, filename);
-        // this.push(new File({
-      //     cwd: opt.cwd,
-      //     base: opt.cwd,
-      //     path: p
-      //   }));
-     //  }.bind(this));
-
-     //  // Done - XXX handle the optional custom theme dir
-     //  globber.on('end', function(){
-     //    console.warn("ending");
-      //   next();
-     //  }.bind(this));
     };
 
     return through2.obj(processor);
@@ -158,7 +131,13 @@
     };
   };
 
-  var yuidoc = {};
+  var yuidoc = function(parserOptions, generatorOptions){
+    return gutil.combine(
+      yuidoc.parser(parserOptions),
+      yuidoc.reporter(),
+      yuidoc.generator(generatorOptions)
+    )();
+  };
 
   // Yui default, provided reporter
   yuidoc.yuiReporter = wrapReporter(require('./lib/uglyreporter'));
@@ -169,17 +148,9 @@
   // Default to chalk, nicier :)
   yuidoc.reporter = yuidoc.chalkReporter;
 
-  yuidoc.generator = streamBuilder;
+  yuidoc.generator = streamGenerator;
 
   yuidoc.parser = streamParser;
-
-  yuidoc.runner = function(options){
-    return gutil.combine(
-      yuidoc.parser(null, options),
-      yuidoc.reporter(options),
-      yuidoc.generator(options)
-    )();
-  };
 
   // Listening on uncaughtException is plain bad - deny YUI the right to do so
   // https://github.com/joyent/node/issues/2582
